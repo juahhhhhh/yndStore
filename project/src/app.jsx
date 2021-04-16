81d86414cb8fb623291262571fde793d221da66d
juah <juah@yanadoocorp.com>
2021-04-17 01:36:06 +0900

    react Setting - 03

--- project/src/app.jsx
+++ project/src/app.jsx
@@ -1,10 +1,12 @@
-import './App.css';
+import './static/app.css';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+  <>
+    <header />
+    <contents />
+    <footer />
+  </>
   );
 }
 
